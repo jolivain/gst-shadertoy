6ae2bf51e27b6c2ff0f20274f32c7ea7ae058334
Julien Olivain <juju@cotds.org>
2016-08-14 18:02:24 +0200

    gst-shadertoy.sh: handle shader file name argument
    Signed-off-by: Julien Olivain <juju@cotds.org>

--- gst-shadertoy.sh
+++ gst-shadertoy.sh
@@ -1,9 +1,18 @@
 #! /bin/bash
 
-gst-launch-1.0 -v \
+if [ "$#" -ne 1 ] ; then
+    echo "Usage: $0 <shader-file>" >&2
+    exit 1
+fi
+
+set -eu
+
+SHADER_FILE="$1"
+
+exec gst-launch-1.0 -v \
   v4l2src device=/dev/video0 ! 'video/x-raw,width=640,height=480,framerate=30/1' ! \
   videoscale ! 'video/x-raw,width=128,height=96,framerate=30/1' ! \
   glupload ! \
   glcolorconvert ! \
-  glshader vertex="\"$(cat myshader.vs)\"" fragment="\"$(cat myshader.fs | sed 's/\"/\\"/g' )\""  ! \
+  glshader vertex="\"$(cat myshader.vs)\"" fragment="\"$(cat "$SHADER_FILE" | sed 's/\"/\\"/g' )\""  ! \
   glimagesink
