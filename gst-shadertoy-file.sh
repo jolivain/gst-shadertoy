97c97f8ffb67eafa15b1cb7b2bc45180a3cd8e88
Julien Olivain <juju@cotds.org>
2016-08-15 11:53:02 +0200

    Add gst-shadertoy-file.sh
    Signed-off-by: Julien Olivain <juju@cotds.org>

---
+++ gst-shadertoy-file.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+if [ "$#" -ne 2 ] ; then
+    echo "Usage: $0 <video-file> <shader-file>" >&2
+    exit 1
+fi
+
+set -eu
+
+VIDEO_FILE="$1"
+SHADER_FILE="$2"
+
+exec gst-launch-1.0 -v \
+  filesrc location="${VIDEO_FILE}" ! \
+  decodebin ! \
+  videoscale ! 'video/x-raw,width=64,height=48' ! \
+  glupload ! \
+  glcolorconvert ! \
+  glshader vertex="\"$(cat myshader.vs)\"" fragment="\"$(cat "$SHADER_FILE" | sed 's/\"/\\"/g' )\""  ! \
+  glimagesink
